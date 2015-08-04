2d080100721be1ab92241b6d6adcb0947c52fcdf
aoki <aoki@cubit.jp>
2015-08-04 19:35:21 +0900

    webpack-streamを実装

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,31 @@ var gulp=require('gulp');
 var plumber = require("gulp-plumber");
 var sass = require('gulp-ruby-sass');
 var pleeease = require('gulp-pleeease');
+var webpack = require('webpack-stream');
 
 // ソースディレクトリとか、ファイル名を指定。
 var paths = {
   "scssSrc": "./sass",
   "scssSrc2": "./sass/*.scss",
-  "cssSrc": "./root/css"
+  "cssSrc": "./root/css",
+  "jsSrc": "./src/*.js",
+  "jsSrc2": "./src/*.js",
+  "jsDistSrc": "./root/js"
 }
+
+//webpack
+gulp.task('webpack',function(){
+    gulp.src([paths.jsSrc])
+    .pipe(webpack({
+      output: {
+        filename: 'main.js',
+      },
+      resolve: {
+        extensions: ['', '.js']
+      },
+     }))
+    .pipe(gulp.dest(paths.jsDistSrc))
+});
 
 
 //sass
@@ -30,6 +48,7 @@ gulp.task('sass', function () {
 });
 
 //watch
-gulp.task('watch', ['sass'], function(){
-	gulp.watch(paths.scssSrc2,["sass"]);
+gulp.task('watch', ['sass','webpack'], function(){
+  gulp.watch(paths.scssSrc2,["sass"]);
+  gulp.watch(paths.jsSrc2,["webpack"]);
 });
